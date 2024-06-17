9c6950dc1f79f0b24d6b5f4514b9356d1cde51f9
Leonardo Geja <bemmenos@gmail.com>
2024-06-17 18:37:50 -0300

    chore: add custom tailwind style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#0989ff",
+        topHeadingPrimary: "##010f1c",
+        topHeadingSecondary: "#021d35",
+        pink: "#FD4B6B",
+      },
+      container: {
+        center: true,
+        padding: "15px"
+      }
+    },
   },
   plugins: [],
 }
-
