e1d591d875cd328ac63e00cdf1465885c35e5cf1
leoanrdo <programacao@studioalpha.com.br>
2024-06-19 14:21:26 -0300

    fix: adjuste navbar, remove input search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import { Feature } from "../components/Feature"
-import { Hero } from "../components/Hero"
 import { TrendingFood } from "../components/TrendingFood"
 
 export const Home = () => {
     return (
         <>
-            <Hero/>
             <Feature/>
             <TrendingFood/>
         </>
