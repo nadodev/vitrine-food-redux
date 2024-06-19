c3759fd5f33b04b2c9f02b1d9533a5a6ea011957
leoanrdo <programacao@studioalpha.com.br>
2024-06-19 13:16:56 -0300

    fix: adjuste cartSLice for save to storage

--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -1,39 +1,52 @@
-import  { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface IProduct {
-    id: number;
-    title : string;
-    img: string;
-    price: number;
-    quantity: number;
-
+  id: number;
+  title: string;
+  img: string;
+  price: number;
+  quantity: number;
 }
 
-const initialState: Array<IProduct> = [];
+const getInitialCartState = (): Array<IProduct> => {
+  const storedCart = localStorage.getItem("cart");
+  return storedCart ? JSON.parse(storedCart) : [];
+};
 
+const initialState: Array<IProduct> = getInitialCartState();
+
+const saveCartToLocalStorage = (cart: Array<IProduct>) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
 
 export const cartSlice = createSlice({
-    name: "cartSlice",
-    initialState,
-    reducers: {
-        addToCart: (state, action: PayloadAction<IProduct>) => {
-            if(state.findIndex((product) => product.id === action.payload.id) === -1) {
-                state.push(action.payload)
-            }else {
-                return state.map((item) => {
-                    return item.id === action.payload.id ? { ...item, quantity: item.quantity + 1} : item;
-                })
-            }
-        },
-
-        removeFromCart: (state, action: PayloadAction<number>) => {
-            const id = action.payload
-            return state.filter((item) => item.id !== id)
-        }
-    }
-
-})
+  name: "cartSlice",
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      const productIndex = state.findIndex(
+        (product) => product.id === action.payload.id
+      );
+      let newState;
+      if (productIndex === -1) {
+        newState = [...state, action.payload];
+      } else {
+        newState = state.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+      saveCartToLocalStorage(newState);
+      return newState;
+    },
+    removeFromCart: (state, action: PayloadAction<number>) => {
+      const newState = state.filter((item) => item.id !== action.payload);
+      saveCartToLocalStorage(newState);
+      return newState;
+    },
+  },
+});
 
 export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
